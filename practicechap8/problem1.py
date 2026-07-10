ae61d28a2fbaf7f96e85c2b990b03511c001e595
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-09 17:40:28 -0700

    the git push are completed

---
+++ practicechap8/problem1.py
@@ -0,0 +1,4 @@
+def print_name():
+    print("Bishesh Ghimire")
+
+print_name()
---
+++ practicechap8/problem6.py
@@ -0,0 +1,8 @@
+def check(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(check(8))
+print(check(7)) 
