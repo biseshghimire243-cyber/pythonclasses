2a95dae5454c5a065391124d7858e9fc6930948b
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-12 23:11:17 -0700

    the introducton are done completed

---
+++ chapter11/interoduction.py
@@ -0,0 +1,20 @@
+# Topic 1: What is Inheritance?
+
+# Inheritance is a feature where one class (child class) acquires the properties and methods of another class (parent class).
+
+# Real-Life Example
+#           Animal
+#              │
+#       ----------------
+#       │              │
+#      Dog            Cat
+# Dog is an Animal.
+# Cat is an Animal.
+
+# Both inherit common features from Animal.
+
+# Advantages
+# Code Reusability
+# Easy Maintenance
+# Less Code Duplication
+# Better Organization
