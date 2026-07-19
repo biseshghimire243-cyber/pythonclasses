9f353931ba6429f8985cbfac0536489452c918b8
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-19 05:09:33 -0700

    the handeling block

---
+++ chapter12/multiple_blocks.py
@@ -0,0 +1,9 @@
+try:
+    num = int(input("Enter a number: "))
+    print(10 / num)
+
+except ValueError:
+    print("Invalid number.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
