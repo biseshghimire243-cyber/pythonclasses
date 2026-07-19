7fe60a840d25e1cbf07d200fcab0ce8f128d2ac2
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-19 05:10:29 -0700

    the else block is created

---
+++ chapter12/else_blocks.py
@@ -0,0 +1,9 @@
+try:
+    num = int(input("Enter a number: "))
+    print(10 / num)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+else:
+    print("Program executed successfully.")
