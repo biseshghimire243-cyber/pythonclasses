bb41a347b0758f86b72139d503adadad19cc0afb
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-26 22:21:26 -0700

    Build login system with three attempts

---
+++ practicequesstion/problem28.py
@@ -0,0 +1,10 @@
+correct_password = "python123"
+
+for attempt in range(3):
+    password = input("Enter password: ")
+
+    if password == correct_password:
+        print("Login Successful")
+        break
+else:
+    print("Account Locked")
