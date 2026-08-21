fda841c8fcc743862044dcda8749bb005d7d0c65
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-21 04:19:04 -0700

    Add vowel and consonant counter

---
+++ chapter21/program27.py
@@ -0,0 +1,13 @@
+text = input("Enter a sentence: ")
+
+vowels = 0
+consonants = 0
+
+for char in text.lower():
+    if char in "aeiou":
+        vowels += 1
+    elif char.isalpha():
+        consonants += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
