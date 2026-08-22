2df80baeeac8c230966c687c7d28a493b2d8f1eb
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-22 01:16:20 -0700

    initial commit

---
+++ adding_section/number_pattern.py
@@ -0,0 +1,7 @@
+numbers = [12, 45, 23, 67, 89, 34, 56]
+
+print("Numbers:", numbers)
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sum:", sum(numbers))
+print("Average:", sum(numbers) / len(numbers))
