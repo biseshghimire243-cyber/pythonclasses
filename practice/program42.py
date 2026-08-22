80c263f3228a70ee430d12b06bd9362a7d94d720
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-21 18:17:28 -0700

    Add character frequency program

---
+++ practice/program42.py
@@ -0,0 +1,12 @@
+text = input("Enter a string: ")
+
+frequency = {}
+
+for char in text:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+for char, count in frequency.items():
+    print(char, ":", count)
