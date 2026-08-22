45022663abc2f688210685ce900a1f566283d693
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-22 01:17:20 -0700

    initial commit

---
+++ adding_section/student_grade.py
@@ -0,0 +1,13 @@
+text = input("Enter a sentence: ")
+
+words = text.lower().split()
+
+frequency = {}
+
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+
+print("\nWord Frequency:")
+
+for word, count in frequency.items():
+    print(word, ":", count)
