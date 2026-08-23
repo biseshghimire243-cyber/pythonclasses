f2f834c24e73181bd9d04df0d1903abfd7c7022e
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-23 04:19:11 -0700

    initial commit

---
+++ manganetrelated/countdown_timer.py
@@ -0,0 +1,11 @@
+numbers = [45, 12, 89, 23, 67, 5, 34]
+
+print("Original list:", numbers)
+
+numbers.sort()
+
+print("Ascending order:", numbers)
+
+numbers.reverse()
+
+print("Descending order:", numbers)
