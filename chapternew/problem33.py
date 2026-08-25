eb893e6cf4797abc278e32aa4a3c697e5c778dc1
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-25 06:01:28 -0700

    initial commit

---
+++ chapternew/problem33.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 20, 40, 10, 50, 30]
+
+duplicates = []
+
+for number in numbers:
+    if numbers.count(number) > 1 and number not in duplicates:
+        duplicates.append(number)
+
+print("Duplicates:", duplicates)
