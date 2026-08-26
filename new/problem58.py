80ef3705adfe5347317ea7b004c3550038af8c41
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-25 18:58:54 -0700

    initial commit

---
+++ new/problem58.py
@@ -0,0 +1,8 @@
+numbers = [25, 67, 12, 89, 45, 91, 34]
+
+maximum = max(numbers)
+position = numbers.index(maximum)
+
+print("Numbers:", numbers)
+print("Maximum:", maximum)
+print("Index:", position)
