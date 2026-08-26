69a77faa4c7a6ec1e5e3450623a5e1b4b4bfee39
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-25 18:58:05 -0700

    initial commit

---
+++ new/problem53.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping:")
+print("A =", a)
+print("B =", b)
+
+a, b = b, a
+
+print("After swapping:")
+print("A =", a)
+print("B =", b)
