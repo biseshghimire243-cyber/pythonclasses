c7c2f62553c99c388cc1802c2b057dd6b02221e9
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-27 02:15:51 -0700

    initial commit

---
+++ logicaldata/problem62.py
@@ -0,0 +1,12 @@
+numbers = [10, 20, 30, 40, 50]
+
+total = 0
+
+for number in numbers:
+    total += number
+
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Total:", total)
+print("Average:", average)
