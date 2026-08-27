27c0545ac98a510dd2aea29119003296b8529caa
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-27 02:17:27 -0700

    initial commit

---
+++ logicaldata/problem70.py
@@ -0,0 +1,9 @@
+numbers = [45, 12, 78, 23, 9]
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+
+        if numbers[i] > numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+print("Sorted data:", numbers)
