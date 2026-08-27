0471813cd6737710b857a4259ba0062b88b30f29
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-27 02:16:29 -0700

    initial commit

---
+++ logicaldata/problem65.py
@@ -0,0 +1,7 @@
+numbers = [25, 89, 45, 67, 91, 34, 78]
+
+unique_numbers = list(set(numbers))
+unique_numbers.sort()
+
+print("Largest:", unique_numbers[-1])
+print("Second Largest:", unique_numbers[-2])
