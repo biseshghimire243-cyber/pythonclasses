7b75bcb58032c071bd0631e650cd715e14eee1b9
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-28 00:07:40 -0700

    initial commit

---
+++ newdata/problem94.py
@@ -0,0 +1,12 @@
+units = float(input("Enter electricity units: "))
+
+if units <= 50:
+    bill = units * 5
+elif units <= 150:
+    bill = (50 * 5) + ((units - 50) * 7)
+elif units <= 250:
+    bill = (50 * 5) + (100 * 7) + ((units - 150) * 10)
+else:
+    bill = (50 * 5) + (100 * 7) + (100 * 10) + ((units - 250) * 12)
+
+print("Electricity Bill:", bill)
