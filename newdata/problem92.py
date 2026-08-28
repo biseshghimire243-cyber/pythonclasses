8e0d7a663e9e063e6f9a7c33a5a87cfb024615ef
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-28 00:07:21 -0700

    initial commit

---
+++ newdata/problem92.py
@@ -0,0 +1,20 @@
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+print("Prime numbers:")
+
+for number in range(start, end + 1):
+
+    if number < 2:
+        continue
+
+    prime = True
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(number, end=" ")
+        
