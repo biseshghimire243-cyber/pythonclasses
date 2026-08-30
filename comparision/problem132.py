595af0e46fb7128121efc134fa345ed5a5ebb857
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-30 04:18:14 -0700

    initial commit

---
+++ comparision/problem132.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Number is greater than zero")
+elif number < 0:
+    print("Number is less than zero")
+else:
+    print("Number is exactly zero")
