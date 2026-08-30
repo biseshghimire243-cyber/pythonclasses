ea1abfe6f65d898bce36d5b3f9a4d56376d2498c
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-30 04:15:25 -0700

    initial commit

---
+++ comparision/problem123.py
@@ -0,0 +1,9 @@
+age1 = int(input("Enter first person's age: "))
+age2 = int(input("Enter second person's age: "))
+
+if age1 > age2:
+    print("First person is older")
+elif age1 < age2:
+    print("Second person is older")
+else:
+    print("Both are the same age")
