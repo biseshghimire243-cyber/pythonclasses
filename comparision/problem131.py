80326dabe559dda7326addcf1933baadb76a89f6
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-30 04:18:01 -0700

    initial commit

---
+++ comparision/problem131.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Excellent")
+elif marks >= 80:
+    print("Very Good")
+elif marks >= 70:
+    print("Good")
+elif marks >= 60:
+    print("Average")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
