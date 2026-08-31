195602635265f6d72bb525f0b1194e9ea276e92f
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-31 07:21:07 -0700

    initial commit

---
+++ premutavedata/problem155.py
@@ -0,0 +1,19 @@
+number = int(input("Enter a number: "))
+
+original = number
+total = 0
+
+while number > 0:
+    digit = number % 10
+
+    factorial = 1
+    for i in range(1, digit + 1):
+        factorial *= i
+
+    total += factorial
+    number //= 10
+
+if total == original:
+    print("Strong number")
+else:
+    print("Not a strong number")
