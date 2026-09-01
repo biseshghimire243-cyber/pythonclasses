182e80711d6dc094889c97540f67253af53be70a
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-01 07:27:34 -0700

    the data arre made and managed

---
+++ virtualmanagement/problem173.py
@@ -0,0 +1,11 @@
+numbers = [15, 25, 35, 45, 55]
+
+total = 0
+
+for number in numbers:
+    total += number
+
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Average:", average)
