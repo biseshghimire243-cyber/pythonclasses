32158b369134482de89ef4c89696caf9d75d5c62
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-10 03:57:28 -0700

    initial commit

---
+++ comparisionfunction/problem317.py
@@ -0,0 +1,13 @@
+text = input("Enter a sentence: ").lower()
+words = text.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+
+for word, count in frequency.items():
+    print(word, ":", count)
