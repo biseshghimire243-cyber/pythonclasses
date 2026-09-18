5ff0d64a10c10b2affaedcfa3b2ae6d20f41acc1
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-18 05:30:46 -0700

    initial commit

---
+++ python-problem-solving/problem454.py
@@ -0,0 +1,6 @@
+total_units = float(input("Enter total units used: "))
+days = int(input("Enter number of days: "))
+
+average = total_units / days
+
+print("Average units per day:", round(average, 2))
