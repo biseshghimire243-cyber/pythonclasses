41ca050f367a306e373d053fac40150babe1455e
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-18 05:29:58 -0700

    initial commit

---
+++ python-problem-solving/problem451.py
@@ -0,0 +1,21 @@
+text = input("Enter a string: ")
+
+letters = 0
+digits = 0
+spaces = 0
+others = 0
+
+for char in text:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+    elif char.isspace():
+        spaces += 1
+    else:
+        others += 1
+
+print("Letters:", letters)
+print("Digits:", digits)
+print("Spaces:", spaces)
+print("Other characters:", others)
